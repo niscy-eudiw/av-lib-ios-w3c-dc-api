2e541ef21035194c2ac52dc428be8ab5c2181d04
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-03-05 16:14:30 +0200

    Update eudi-lib-ios-iso18013-data-transfer dependency to version 0.10.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git", exact: "0.10.0"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git", exact: "0.10.1"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", exact: "0.10.0"),
 		.package(url: "https://github.com/leif-ibsen/SwiftHPKE", from: "2.8.0"),
 	],
